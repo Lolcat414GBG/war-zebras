b0ae5018b1b24d9eebc4f05c3b15a65c58ef6a8d
jensengbg-linda-jansson <linda.t.jansson@gmail.com>
2021-03-29 13:55:13 +0200

    Updated routes och views

--- airbean/src/router/index.js
+++ airbean/src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Landing from '../views/Landing.vue'
+import Menu from '../views/Menu.vue'
+import Cart from '../views/Cart.vue'
+import Status from '../views/Status.vue'
+import Profile from '../views/Profile.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +16,28 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/landing',
+    name: 'Landing',
+    component: Landing
+  },
+  {
+    path: '/menu',
+    name: 'Menu',
+    component: Menu
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
+  },
+  {
+    path: '/status',
+    name: 'Status',
+    component: Status
+  },{
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
   }
 ]
 
