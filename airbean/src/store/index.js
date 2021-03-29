3a60f95537b99e848c3e619924ea6ebafd295bbc
william-kakela <70516535+william-kakela@users.noreply.github.com>
2021-03-29 16:48:50 +0200

    fetch Get

--- airbean/src/store/index.js
+++ airbean/src/store/index.js
@@ -3,13 +3,37 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const url = 'http://localhost:3000/api/beans';
+
 export default new Vuex.Store({
   state: {
+    menu: []
   },
   mutations: {
+    setMenu(state, menu) {
+      state.menu = menu
+    }
   },
   actions: {
+    async fetchMenu(ctx) {
+      const response = await fetch(url, {
+        method: 'GET', headers: {
+          'Content-Type': 'application/json'
+
+        },
+      });
+      const data = await response.json();
+      ctx.commit('setMenu', data.menu)
+      console.log(data)
+
+    }
   },
+  getters: {
+    getMenu(state) {
+      return state.menu
+    }
+  },
+
   modules: {
   }
 })
--- airbean/src/views/Menu.vue
+++ airbean/src/views/Menu.vue
@@ -1,13 +1,53 @@
 <template>
-  <p>Menu</p>
+  <main>
+    <h1>Meny</h1>
+    <ul>
+      <li v-for="coffee in menu" :key="coffee.id">
+        <button class="add_coffee"></button>
+        <div>
+          <h2>{{ coffee.title }}</h2>
+          <p>{{ coffee.desc }}</p>
+        </div>
+        <span> {{ coffee.price }} kr</span>
+      </li>
+    </ul>
+  </main>
 </template>
 
 <script>
 export default {
-
-}
+  name: "CoffeMenuComponent",
+  created() {
+    this.fetchMenuFromApi();
+  },
+  computed: {
+    menu() {
+      return this.$store.getters.getMenu;
+    },
+  },
+  methods: {
+    async fetchMenuFromApi() {
+      await this.$store.dispatch("fetchMenu");
+    },
+  },
+};
 </script>
 
 <style>
+.add_coffee {
+  background-image: url("../assets/add.svg");
+  width: 20px;
+  height: 20px;
+  background-color: black;
+  border-radius: 50%;
+}
+ul {
+  list-style: none;
+}
 
+li {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 </style>
