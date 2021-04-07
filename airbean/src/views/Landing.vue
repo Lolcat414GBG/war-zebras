960eb0b16e79bff6696b9c4e183c6e5269c81cd8
Linda Jansson <linda.t.jansson@gmail.com>
2021-04-07 13:38:53 +0200

    Update Landing.vue

--- airbean/src/views/Landing.vue
+++ airbean/src/views/Landing.vue
@@ -1,27 +1,19 @@
 <template>
   <div class="landing">
-    <!--<img class="landingLeaf__left" v-bind:src= "leafleft.leafleft" alt="leaf">-->
-
     <router-link class="logo" to="/menu">
       <img v-bind:src="landingpage.landingpage" alt="landingpage" />
     </router-link>
-
-    <!--<img class="landingLeaf__right" v-bind:src= "leafright.leafright" alt="leaf">-->
   </div>
 </template>
 
 <script>
 import airbeanLanding from "../assets/airbean-landing.svg";
-//import leafLeft from "../assets/intro-graphic-left.svg"
-//import leafRight from "../assets/intro-graphic-right.svg"
 
 export default {
   components: {},
   data() {
     return {
       landingpage: { landingpage: airbeanLanding },
-      //leafleft: {'leafleft': leafLeft},
-      //leafright: {'leafright': leafRight}
     };
   },
 };
@@ -41,11 +33,5 @@ export default {
 .logo {
   align-self: center;
   margin: -70px;
-}
-
-.landingLeaf__left {
-  height: 100%;
-  width: 30%;
-  align-self: flex-end;
 }
 </style>
